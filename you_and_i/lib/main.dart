b8d3775104af2f8d7cf68aeb5b63c9929c84ee1f
jinseon5462 <dlwlstjs5462@gmail.com>
2023-03-18 09:47:25 +0900

    2023/03/18 기념일 앱

---
+++ you_and_i/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:you_and_i/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
