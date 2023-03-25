c983aa5c0cd75183117c3cf52c6d0e717bfba267
jinseon5462 <dlwlstjs5462@gmail.com>
2023-03-25 22:42:53 +0900

    2023/03/25 - 비디오 플레이어 구현

---
+++ vid_player/lib/component/custom_video_player.dart
@@ -0,0 +1,143 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+
+class CustomVideoPlayer extends StatefulWidget {
+  final XFile video;
+  const CustomVideoPlayer({
+    required this.video,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  VideoPlayerController? videoController;
+
+  @override
+  void initState() {
+    super.initState();
+    initialLizeController();
+  }
+
+  initialLizeController() async {
+    videoController = VideoPlayerController.file(
+      File(widget.video.path),
+    );
+    await videoController!.initialize();
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (videoController == null) {
+      return CircularProgressIndicator();
+    }
+
+    return AspectRatio(
+      aspectRatio: videoController!.value.aspectRatio,
+      child: Stack(
+        children: [
+          VideoPlayer(
+            videoController!,
+          ),
+          _Controlls(
+            onReversePressed: onReversePressed,
+            onPlayPressed: onPlayPressed,
+            onForwardPressed: onForwardPressed,
+            isPlaying: videoController!.value.isPlaying,
+          ),
+          Positioned(
+            // Stack위젯에서 많이 사용하는 Positioned widget
+            right: 0, // 오른쪽 끝으로 배치
+            child: IconButton(
+              onPressed: () {},
+              color: Colors.white,
+              iconSize: 30.0,
+              icon: Icon(Icons.photo_camera_back),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void onReversePressed() {
+    final currentPosition = videoController!.value.position;
+    Duration position = currentPosition - Duration(seconds: 3);
+    videoController!.seekTo(position);
+  }
+
+  void onForwardPressed() {}
+
+  void onPlayPressed() {
+    // 이미 실행중이면 실행중지
+    // 실행중이 아니라면 실행
+    setState(() {
+      if (videoController!.value.isPlaying == true) {
+        videoController!.pause();
+      } else {
+        videoController!.play();
+      }
+    });
+  }
+}
+
+class _Controlls extends StatelessWidget {
+  final VoidCallback onPlayPressed;
+  final VoidCallback onReversePressed;
+  final VoidCallback onForwardPressed;
+  final bool isPlaying;
+
+  const _Controlls({
+    required this.onPlayPressed,
+    required this.onReversePressed,
+    required this.onForwardPressed,
+    required this.isPlaying,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withOpacity(0.4),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          renderIconButton(
+            onPressed: onReversePressed,
+            iconData: Icons.rotate_left,
+          ),
+          renderIconButton(
+            onPressed: onPlayPressed,
+            iconData: isPlaying ? Icons.pause : Icons.play_arrow,
+          ),
+          renderIconButton(
+            onPressed: onForwardPressed,
+            iconData: Icons.rotate_right,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget renderIconButton({
+    required VoidCallback onPressed,
+    required IconData iconData,
+  }) {
+    return IconButton(
+      onPressed: onPressed,
+      iconSize: 30.0,
+      color: Colors.white,
+      icon: Icon(
+        iconData,
+      ),
+    );
+  }
+}
---
+++ vid_player/lib/screen/home_screen.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:vid_player/component/custom_video_player.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // XFile은 이미지 픽커에서 제공해주는 파일의 클래스이다.
+  // XFile형태로 모든 이미지와 영상을 리턴받을 수 있다.
+  XFile? video;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: video == null ? renderEmpty() : renderVideo(),
+    );
+  }
+
+  Widget renderVideo() {
+    return Center(
+      child: CustomVideoPlayer(
+        video: video!,
+      ),
+    );
+  }
+
+  Widget renderEmpty() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: getBoxDecoration(),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          _Logo(
+            onTap: onLogoTap,
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          _AppName(),
+        ],
+      ),
+    );
+  }
+
+  void onLogoTap() async {
+    final video = await ImagePicker().pickVideo(
+      source: ImageSource.gallery,
+    );
+    if (video != null) {
+      setState(() {
+        this.video = video;
+      });
+    }
+  }
+
+  BoxDecoration getBoxDecoration() {
+    return BoxDecoration(
+      gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color(0xff2a3a7c),
+          Color(0xff000118),
+        ],
+      ),
+    );
+  }
+}
+
+class _Logo extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const _Logo({
+    required this.onTap,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Image.asset('asset/image/logo.png'),
+    );
+  }
+}
+
+class _AppName extends StatelessWidget {
+  const _AppName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 30.0,
+      fontWeight: FontWeight.w300,
+    );
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'VIDEO',
+          style: textStyle,
+        ),
+        Text(
+          'PLAYER',
+          style: textStyle.copyWith(fontWeight: FontWeight.w700),
+        ),
+      ],
+    );
+  }
+}
